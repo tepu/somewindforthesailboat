2d500f793d940d74088f6ed0caf3fdc4d955de07
Benjamin Jeffery <benjamin.jeffery@bedarra.com>
2017-03-15 00:53:14 -0400

    game basically finished

--- doff/doff.js
+++ doff/doff.js
@@ -1,48 +1,93 @@
+var normalEncounter = [
+    "you pass",
+    "you see",
+    "you are greeted by",
+    "you chance across",
+    "you are winked at by",
+    "you exchange a knowing glance with",
+    "you reach for the same umbrella as",
+    "you accidentally stare too long at"
+];
+var normalPerson = [
+    "an episcopal bishop",
+    "a soldier on leave",
+    "a lively costermonger",
+    "a constable making their rounds",
+    "a gentleman, with no hat",
+    "a woman in a wide-brimmed hat",
+    "a prominent local politician",
+    "two journalists, having a conversation"
+];
+var aftermathEncounter = [
+    "you are dimly aware of",
+    "you take no notice of",
+    "you are surrounded by",
+    "you press through",
+    "you fight past",
+    "you bump against"
+];
+var aftermathPerson= [
+    "faceless bodies, gawking",
+    "a blur of hollow faces",
+    "a suffocating grey fog",
+    "a sudden stillness",
+    "the gathering crowds",
+    "policemen shouting loudly"
+];
+
 var text = [
-    {   // initial
+    {
         counter: 6,
-        preamble : [
-            "",
+        preamble: [
+            "on the Sabbath",
             "while hunting in Nantes",
-            "during a routine inspection",
             "on your way to tiffin",
             "During the first bowl of the over (at Wembley)",
             "while carrying a cane and volume of Proust",
             "ascending the steps of parliment",
             "while reaching for your cigarette case",
+            "during tiffin on the Seine",
+            "at the green grocers",
+            "on the promenade",
+            "in the foyer of the office",
+            "while finding your seat at the opera",
+            "during the national anthem",
+            "during a lecture on Ovid",
+            "onboard a Brunel's Great Eastern steamer",
+            "while smoking at the club"
         ],
         encounter: [
             "you pass",
             "you see",
             "you are greeted by",
-            "you chance across"
+            "you chance across",
+            "you are noticed by"
         ],
-        person   : [
+        person: [
             "an episcopal bishop",
             "a soldier on leave",
-            "a gentleman, with no hat,",
+            "a lively costermonger",
+            "a constable making their rounds",
+            "a gentleman, with no hat",
             "a woman in a wide-brimmed hat",
             "Her Majesty",
             "a prominent local politician",
-            "two journalists, having a conversation,"
-        ],
-        setting  : [
-            "in the hallway of a hospital",
-            "in the vestibule of a church",
-            "in the foyer of an office",
-            "during tiffin on the Seine",
-            "at the green grocers",
-            "at a parade",
-            "on the promenade"
+            "two journalists, having a conversation",
+            "a bishop in a mitre",
+            "a line of convicts",
+            "a shoeshining waif"
         ]
-    }, {    // wacky
+    }, {
         counter: 6,
-        preamble : [
+        showDoNothing: true,
+        preamble: [
             "after a fit of the whistles",
             "while waiting for a funicular",
             "while entering a funicular",
             "while exiting a funicular",
-            "walking along the quay"
+            "walking along the quay",
+            "in a crowded funicular car",
+            "on the funicular platform"
         ],
         encounter: [
             "you pass",
@@ -54,150 +99,210 @@ var text = [
             "you reach for the same umbrella as",
             "you accidentally stare too long at"
         ],
-        person   : [
-            "an episcopal bishop",
+        person: [
             "Her Majesty",
+            "il principe de Monaco",
             "a Japanese salaryman",
-            "a prominent local politician",
             "local fisherman Ernest Tronk",
             "a prominent local fisherman",
-            "well-known charlatan Ernest Tronk",
-            "a very inebriated funicular repairman"
-        ],
-        setting  : [
-            "in the hallway of a hospital",
-            "in the vestibule of a church",
-            "in a funicular",
-            "in a crowded trolly car",
-            "at the funicular platform"
+            "a procession of mourners",
+            "a gang of tattooed stevedors",
+            "a very inebriated funicular repairman",
+            "Mr. Tronk, enjoying the morning air"
         ]
-    }, {    // tronk
-        counter: 10,
-        preamble : [
+    }, {
+        counter: 9,
+        preamble: [
             "after a fit of the whistles",
             "finding yourself daydreaming,",
             "on the way to work",
             "while waiting for a funicular",
             "while entering a funicular",
-            "while exiting a funicular"
+            "while exiting a funicular",
+            "at the docks",
+            "in a crowded funicular",
+            "in an empty funicular",
+            "at the funicular platform",
+            "in a tavern"
         ],
         encounter: [
             "you smile at",
-            "you exchange gossip",
+            "you exchange gossip with",
             "you are pleased to see",
             "you anticipate meeting",
             "you are winked at by",
             "you exchange a knowing glance with",
             "you reach for the same umbrella as",
-            "you accidentally stare too long at"
+            "you accidentally stare too long at",
+            "you bump into"
         ],
-        person   : [
-            "a rogueish charmer, smelling of the sea,",
+        person: [
+            "that roguish charmer Ernest, smelling of the sea",
             "local fisherman Ernest Tronk",
             "a prominent local fisherman",
-            "well-known charlatan Ernest Tronk",
+            "the salty old sea dog Ernest Tronk",
+            "mariner extraordinare, Ernest Tronk",
+            "a festive Ernest Tronk, dancing a reel",
             "that irrepressible scallywag Ernest Tronk",
-            "a rapscallion of a herring vendor"
-        ],
-        setting  : [
-            "at the docks",
-            "in a crowded funicular",
-            "in an empty funicular",
-            "waiting for the funicular",
-            "in a tavern",
-            "at the funicular platform"
+            "one Ernest Tronk, a rapscallion of a herring trawler",
+            "Mr. Tronk, enjoying the morning air",
+            "that jocund fellow Ernest, up to his old tricks",
+            "paragon of bonhomie Ernest Tronk, laughing his salty laugh"
         ]
-    }, {    // accident
-        counter: 5,
-        preamble : [
-            "arriving late for your funicular",
-            "reaching the funicular just as the doors close",
-            "as the brakes on the funicular fail",
-            "as the cable on the funicular snaps",
-            "as the funicular jumps the tracks",
-            "as the funicular pulley tears away from the face of the cliff"
-        ],
-        encounter: [
-            "you see"
-        ],
-        person   : [
-            "Ernest Tronk"
-        ],
-        setting  : [
-            ""
-        ]
-    }, {    // aftermath
-        counter: 7,
-        preamble : [
-            "frantically looking for another way down the mountain",
-            "upon reaching the bottom of the funicular",
-            "riding a stolen bicycle down the goat path to the foot of the mountain",
-            "standing behind the barricade",
-            "jumping over the barricade"
-        ],
-        encounter: [
-            "you are dimly aware of",
-            "you take no notice of",
-            "you are surrounded by",
-            "you press through"
-        ],
-        person   : [
-            "faceless bodies, gawking",
-            "a blur of hollow faces",
-            "a suffocating grey fog",
-            "a sudden stillness",
-            "the gathering crowds",
-            "policemen shouting loudly"
-        ],
-        setting  : [
-            ""
-        ]
+    }, {
+        counter  : 1,
+        preamble : ["arriving late for your funicular"],
+        encounter: normalEncounter,
+        person   : normalPerson
+    }, {
+        counter  : 1,
+        preamble : ["reaching the funicular just as the doors close"],
+        encounter: normalEncounter,
+        person   : normalPerson
+    }, {
+        counter  : 1,
+        preamble : ["as everyone on the platform begins to shout"],
+        encounter: ["you see"],
+        person   : ["Ernest Tronk"]
+    }, {
+        counter  : 1,
+        preamble : ["as the cable on the funicular snaps"],
+        encounter: ["you see"],
+        person   : ["Ernest Tronk"]
+    }, {
+        counter  : 1,
+        preamble : ["as the brakes on the funicular fail"],
+        encounter: ["you see"],
+        person   : ["Ernest Tronk"]
+    }, {
+        counter  : 1,
+        preamble : ["as the funicular jumps the tracks"],
+        encounter: ["you see"],
+        person   : ["Ernest Tronk"]
+    }, {
+        counter  : 1,
+        preamble : ["frantically looking for another way down the mountain"],
+        encounter: aftermathEncounter,
+        person   : aftermathPerson
+    }, {
+        counter  : 1,
+        preamble : ["riding a stolen bicycle down to the foot of the mountain"],
+        encounter: aftermathEncounter,
+        person   : aftermathPerson
+    }, {
+        counter  : 1,
+        preamble : ["upon reaching the bottom of the funicular"],
+        encounter: aftermathEncounter,
+        person   : aftermathPerson
+    }, {
+        counter  : 1,
+        preamble : ["jumping over the barricade"],
+        encounter: aftermathEncounter,
+        person   : aftermathPerson
+    }, {
+        counter  : 1,
+        preamble : ["unsure how you got home"],
+        encounter: aftermathEncounter,
+        person   : aftermathPerson
     }, {    // funeral
-        counter: 10,
+        counter: 7,
+        hideDoff: true,
         preamble : [
             "at the funeral",
+            "after the funeral",
             "as the mayor declares a day of mourning",
             "walking alone at the wharf",
-            "riding to work in the new funicular"
+            "riding to work in the new funicular",
+            "eating cold food",
+            "coughing at the grocery store",
+            "scraping toast into the sink",
+            "pulling your coat tighter against a cold wind",
+            "looking at the cracked skin on your hands",
+            "you go to bed early lest"
         ],
         encounter: [
             "you stare vacantly at",
-            "you mumble something you immediately forget",
+            "you mumble something you immediately forget to",
             "you try to ignore",
-            "you turn around to avoid the eyes of"
+            "you turn around to avoid the haunted eyes of",
+            "you do not notice",
+            "you think of"
         ],
-        person   : [
+        person: [
             "the elder Mr. Tronk",
             "Ernest's mother",
-            "a repoter",
+            "one of Ernest's sisters",
             "an endless stream of strangers",
-            "the formless shadows"
-        ],
-        setting  : [
-            ""
+            "the formless shadows",
+            "the abyss",
+            "the silent, enveloping darkness",
+            "the hollow men, rats feet over broken glass in a dry cellar",
+            "a vacant, unfeeling god"
         ]
     }
 ];
 
 var actNum = 0;
 var progress = 0;
-function generateSentence () {
+var endStateCounter=0;
+function updateText () {
     var act = text[actNum];
+
+    if (!act) {
+        endStateCounter++;
+        document.getElementById('question').textContent =
+            (endStateCounter % 2) ? "You wake up" : "You go to sleep";
+
+        if (endStateCounter > 3 && endStateCounter <= 13) {
+            document.body.style.opacity = 1 - (0.1 * (endStateCounter - 3));
+        }
+
+        return;
+    }
+
     act.counter--;
     if (act.counter === 0) actNum++;
 
-    document.getElementById('question').textContent =
-        [act.preamble, act.encounter, act.person]
-            .map(_.sample)
-            .join(' ')
-            .trim()
-            .replace(/^./, function (text) {
-                return text.toUpperCase();
-            });
+    progress++;
+    var progressEl = document.getElementById('progress');
+    progressEl.textContent = progress + "/20";
+    if (progress > 7 && progress <= 17) {
+        progressEl.style.opacity = 1 - (0.1 * (progress - 7));
+    }
+
+    if (act.showDoNothing) {
+        act.showDoNothing = false;
+        document.getElementById('doNothing').style.opacity = 1;
+    }
+
+    if (act.hideDoff) {
+        act.hideDoff = false;
+        document.getElementById('doff').style.opacity = 0;
+        document.getElementById('doNotDoff').style.opacity = 0;
+    }
+
+    // Generate a new question that is different from the current question
+    var questionEl = document.getElementById('question');
+    var newText = generateSentence(act);
+    while (questionEl.textContent === newText) {
+        newText = generateSentence(act);
+    }
+
+    questionEl.textContent = newText;
 }
 
+var generateSentence = function (act) {
+    return [act.preamble, act.encounter, act.person]
+        .map(_.sample)
+        .join(' ')
+        .trim()
+        .replace(/^./, function (text) {
+            return text.toUpperCase();
+        });
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    generateSentence();
+    updateText();
 });
 
 
